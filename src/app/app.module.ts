0e36f137113550f3e583ca64a00b8d268b1ca334
vadim <vadimt2@gmail.com>
2020-07-08 20:12:35 +0300

    make it better3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { NavComponent } from './Header/nav/nav.component';
+import { NavComponent } from './header/nav/nav.component';
 import { ProductComponent } from './product/product.component';
 import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ConfirmationOrderComponent } from './confirmation-order/confirmation-order.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
+import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ProductService } from './Services/Products/product.service';
-import { CurrencyPipe, DecimalPipe } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CurrencyPipe } from '@angular/common';
 import { CartService } from './Services/cart/cart.service';
 import { GetCurrencyService } from './services/get-currency/get-currency.service';
 import { ShippingService } from './services/shipping/shipping.service';
-import { ConfirmationOrderComponent } from './confirmation-order/confirmation-order.component';
-import { CheckoutComponent } from './checkout/checkout.component';
 import { OrderService } from './services/order/order.service';
-import { OrderHistoryComponent } from './order-history/order-history.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 
 
 
